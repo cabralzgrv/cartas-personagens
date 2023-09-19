069fc868ced29947656f87cccec8e5ddbc51e973
Lucas Cabral <cabralz18@outlook.com>
2023-09-19 18:53:03 -0300

    project done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import { Personagem } from "./modules/personagens.js"
 import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
+import { Arqueiro } from "./modules/arqueiro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Guerreiro } from "./modules/guerreiro.js"
 
-const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
-const personagemJose = new Personagem('Jose', 3 , 'Arqueiro')
+const magoAntonio = new Mago('Antônio', 4, 'Fogo', 4, 3)
+const magaJulia = new Mago('Julia', 8, 'Gelo', 7, 10)
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
+const guerreiroLucas = new Guerreiro('Lucas', 8)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [personagemPedrinho, personagemJose]
+const personagens = [magaJulia, magoAntonio, arqueiroBruno, arqueiroMagoChico, guerreiroLucas]
 
 new PersonagemView(personagens).render()
-
-console.log(personagemJose.vida)
